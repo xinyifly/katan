28970458e420700deafc6798b6cf7c8b3c817a7a
Zeyu Chen <xinyifly@live.cn>
2019-05-25 18:19:19 +0800

    feat: generate Votes

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,33 @@
+class VotesController < ApplicationController
+  before_action :set_vote, only: [:show]
+  before_action :authenticate_user!, only: [:create]
+
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    candidate = User.find(vote_params[:candidate_id])
+    @vote = current_user.vote(candidate)
+
+    respond_to do |format|
+      if @vote.save
+        format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
+        format.json { render :show, status: :created, location: @vote }
+      else
+        format.html { render :new }
+        format.json { render json: @vote.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_vote
+    @vote = Vote.find(params[:id])
+  end
+
+  def vote_params
+    params.require(:vote).permit(:candidate_id)
+  end
+end
